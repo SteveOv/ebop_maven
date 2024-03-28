caf73552e48b3ae87df234e8271df17f669b9155
SteveOv <steveoverall@hotmail.co.uk>
2024-03-28 16:06:02 +0000

    ported over code for generating trainsets from models

--- ebop_maven/trainsets.py
+++ ebop_maven/trainsets.py
@@ -4,16 +4,21 @@ Generates a synthetic trainset based on parameter distribution functions
 # We use some ucase variable names as they match the equivalent symbol
 # pylint: disable=invalid-name
 
-from typing import Dict, List
+from typing import Dict, List, Iterable
 from pathlib import Path
 from inspect import getsourcefile
 from timeit import default_timer
 from datetime import timedelta
+from itertools import product
+import json
+import random
 
 import numpy as np
 import astropy.units as u
 
-from .libs import param_sets, orbital
+from .libs import param_sets, orbital, limb_darkening
+from .libs.mission import Mission
+from .libs.stellarmodels import StellarModels
 
 _this_dir = Path(getsourcefile(lambda:0)).parent
 
@@ -50,7 +55,7 @@ Generate & write dEB system parameters CSVs from random parameter distributions
 -------------------------------------------------------------------------------
 The number of instances to generate:    {instance_count:,} across {file_count} file(s)
 The parameter CSVs will be written to:  {output_dir}
-The random seed to use when selections: {seed}\n""")
+The random seed to use for selections:  {seed}\n""")
         if simulate:
             print("Simulate requested so no files will be written.\n")
 
@@ -61,10 +66,10 @@ The random seed to use when selections: {seed}\n""")
     # If we decide we need parallel running we'll have to look at setting the seed per file.
     np.random.seed(seed)
     for ix, file_inst_count in enumerate(_calculate_file_splits(instance_count, file_count), 1):
-        out_file = output_dir / f"trainset{ix:02d}.csv"
+        out_file = output_dir / f"trainset{ix:03d}.csv"
         psets = [*generate_instances_from_distributions(file_inst_count, out_file.name, verbose)]
         if verbose:
-            print(f"{'Simulated s' if simulate else 'S'}aving {len(psets)} system(s) to {out_file}")
+            print(f"{'Simulated s' if simulate else 'S'}aving {len(psets)} inst(s) to {out_file}")
         if not simulate:
             param_sets.write_param_sets_to_csv(out_file, psets)
 
@@ -77,7 +82,7 @@ def generate_instances_from_distributions(instance_count: int, label: str, verbo
     Generates the requested number of instances based on random distributions
     system of parameters.
 
-    :instance_count: the number of systems to write to it
+    :instance_count: the number of systems to generate
     :verbose: whether to print out verbose progress/diagnostics information
     :returns: a generator over the dictionaries, one per system
     """
@@ -113,7 +118,7 @@ def generate_instances_from_distributions(instance_count: int, label: str, verbo
         ecosw       = ecc * np.cos(omega_rad)
         rA          = rA_plus_rB / (1 + k)
         rB          = rA_plus_rB - rA
-        imp_params  = orbital.impact_parameter(rA, inc, ecc, None, esinw, orbital.EclipseType.BOTH)
+        imp_prm     = orbital.impact_parameter(rA, inc, ecc, None, esinw, orbital.EclipseType.BOTH)
 
         # Create the pset dictionary.
         generated_counter += 1
@@ -138,8 +143,8 @@ def generate_instances_from_distributions(instance_count: int, label: str, verbo
             "rB":           rB,
             "e":            ecc,
             "omega":        omega.to(u.deg).value,
-            "bP":           imp_params[0],
-            "bS":           imp_params[1],                      
+            "bP":           imp_prm[0],
+            "bS":           imp_prm[1],                      
         }
 
         if _is_usable_system(pset):
@@ -147,7 +152,234 @@ def generate_instances_from_distributions(instance_count: int, label: str, verbo
             usable_counter += 1
             if verbose and usable_counter % 100 == 0:
                 print(f"{label}: Generated {usable_counter:,} usable",
-                      f"systems from {generated_counter:,} distinct configurations.")
+                      f"instances from {generated_counter:,} distinct configurations.")
+
+
+def write_trainset_from_models(pspace_file: Path,
+                               output_dir: Path,
+                               mission_name: str="TESS",
+                               models_name: str="MIST",
+                               drop_ratio: float=0.5,
+                               seed: float=42,
+                               verbose: bool=False,
+                               simulate: bool=False) -> None:
+    """
+    Writes trainset csv file(s) with instances created by using stellar models
+    to generate plausible binary systems from every combination in the input
+    parameter space.
+
+    :psapce_file: the input parameter space json file
+    :output_dir: the directory to write the files to
+    :mission_name: name of the observing mission; TESS or Kepler 
+    :models_name: name of the stellar models to use; MIST or Parsec
+    :drop_ratio: the ratio of generated models to randomly discard
+    :seed: random seed to ensure repeatability
+    :verbose: whether to print verbose progress/diagnostic messages
+    :simulate: whether to simulate the process, skipping only file/directory actions
+    """
+    if not output_dir:
+        output_dir = Path("~/datasets/formal-trainset/").expanduser()
+    if not simulate:
+        output_dir.mkdir(parents=True, exist_ok=True)
+
+    if verbose:
+        print(f"""
+Generate & write dEB system parameters CSVs from random parameter distributions
+-------------------------------------------------------------------------------
+The systems' parameter space read from: {pspace_file}
+The parameter CSVs will be written to:  {output_dir}
+The observation mission to mimic:       {mission_name}
+The stellar models to lookup:           {models_name}
+The ratio of instances to discard:      {drop_ratio}
+The random seed to use when selections: {seed}\n""")
+        if simulate:
+            print("Simulate requested so no files will be written.\n")
+
+    if verbose:
+        start_time = default_timer()
+    models = StellarModels.get_instance(models_name)
+    mission = Mission.get_instance(mission_name)
+
+    # Read in the parameter space
+    with open(pspace_file, "r", encoding="utf8") as f:
+        pspace = json.load(f)["pspace"]
+    pspace["initial_mass"] *= u.solMass
+    pspace["period"] *= u.d
+    pspace["inc"] *= u.deg
+    pspace["omega"] *= u.deg
+
+    # Start by generating distinct pairs of stars; masses, age and metallicity (Z & Y)
+    stellar_psets = []
+    for Z in pspace["Z"]:
+        Y = models.list_distinct_y_values(z_filter=Z)[0] # Expect 1-to-1 relation between Z & Y
+        #print(f"Available initial masses: ", parsec.list_distinct_initial_masses())
+        initial_masses = sorted(pspace.get("initial_mass", []), reverse=True)
+        countback_MB = pspace.get("count_lower_MB_masses", 0)
+
+        for ix_MA, init_MA in enumerate(initial_masses, start=0):
+            # The range of B star masses starts at MA and descends "countback" steps.
+            # Then find ages applicable to both; limited by the more massive star.
+            init_MBs = initial_masses[ix_MA : ix_MA + max([1, 1 + countback_MB])]
+            max_mass = max(init_MA, max(init_MBs)) # Not in this case pylint: disable=nested-min-max
+            all_ages = sorted(models.list_distinct_ages(max_mass))
+            if max_mass <= 1. * u.solMass: # TODO: models have log10 ages so algo could be refined
+                ages = [9.5] * u.dex(u.yr) # ~ 3.5 Gyr
+            else:
+                ages = [all_ages[int(np.ceil(len(all_ages)*.9))]] * u.dex(u.yr) # tends to later M-S
+
+            if verbose:
+                phases = [models.lookup_parameters(Z, Y, init_MA, a, ["PHASE"])[0] for a in ages]
+                print(f"{models_name} models for mass {max_mass} cover ages ({min(all_ages):.2f},",
+                      f"{max(all_ages):.2f}). Will use: {','.join(f'{a:.2f}' for a in ages)}",
+                      f"(at phases: {','.join(f'{p:.2f}' for p in phases)}).")
+            stellar_psets += [*product([Z], [Y], [init_MA], init_MBs, ages)]
+
+        if verbose:
+            print(f"\nGenerated {len(stellar_psets)} unique pairs of stars. Will now",
+                   "write a trainset csv for each pair by varying their configuration.")
+        for ix, (z, y, ima, imb, age) in enumerate(stellar_psets, start=1):
+            out_file = output_dir / f"trainset{ix:03d}.csv"
+            label = out_file.name
+            psets = [*generate_instances_from_models(z, y, ima, imb, age,
+                                                     pspace["period"],
+                                                     pspace["inc"],
+                                                     pspace["ecc"],
+                                                     pspace["omega"],
+                                                     pspace["L3"],
+                                                     mission, models, label, verbose)]
+            save_ct = pset_ct = len(psets)
+            if drop_ratio and pset_ct:
+                # Discard the requested proportion of the systems generated, selected at random
+                save_ct = round(pset_ct * (1 - drop_ratio))
+                save_ixs = sorted(random.Random(f"{label}/{seed}").sample(range(pset_ct), save_ct))
+                if verbose:
+                    print(f"Retaining {save_ct} of {pset_ct} instance(s) (drop ratio {drop_ratio})")
+
+            if not simulate:
+                if save_ct != pset_ct:
+                    cols = psets[0].keys() # Generator, so we need to give _write the field names
+                    param_sets.write_param_sets_to_csv(out_file, (psets[i] for i in save_ixs), cols)
+                elif save_ct:
+                    param_sets.write_param_sets_to_csv(out_file, psets)
+                elif verbose:
+                    print("Nothing to save for ", out_file)
+
+            if verbose:
+                print(f"{'Simulated s' if simulate else 'S'}aving {save_ct} inst(s) to", out_file)
+
+    if verbose:
+        print(f"\nFinished. The time taken was {timedelta(0, round(default_timer()-start_time))}.")
+
+
+@u.quantity_input
+def generate_instances_from_models(z: float,
+                                   y: float,
+                                   init_MA: u.solMass,
+                                   init_MB: u.solMass,
+                                   age: u.dex(u.yr), # type: ignore
+                                   periods: Iterable[u.Quantity[u.d]],
+                                   incs: Iterable[u.Quantity[u.deg]],
+                                   eccs: Iterable[float],
+                                   omegas: Iterable[u.Quantity[u.deg]],
+                                   L3s: Iterable[float],
+                                   mission: Mission,
+                                   models: StellarModels,
+                                   label: str,
+                                   verbose: bool=True):
+    """
+    Generates the full set of systems for the fixed parameters Z, Y,
+    initial masses and age. The pset is populated by taking the Cartesian
+    product of the lists of orbital parameters and third lights supplied.
+
+    :z: the metallicity Z value
+    :y: the metallicity Y value
+    :initial_MA: the initial mass of the primary star in solMass
+    :initial_MB: the initial mass of the secondary star in solMass
+    :age: the log10(age) of both stars in dex(yr)
+    :periods: list of orbital periods to model
+    :incs: list of orbital inclinations to model
+    :eccs: list of orbital eccentricities to model
+    :omegas: list of orbital arguments of periastron to model
+    :L3s: list of third light values to model
+    :mission: the observation mission the mimic
+    :models: the source of stellar model data
+    :label: to include in messages
+    :returns: a generator over the dictionaries, one per system
+    """
+    usable_counter = generated_counter = 0
+
+    MA, RA, T_eff_A, LA, loggA = models.lookup_stellar_parameters(z, y, init_MA, age)
+    MB, RB, T_eff_B, LB, loggB = models.lookup_stellar_parameters(z, y, init_MB, age)
+    k = (RB / RA).value
+    q = (MB / MA).value
+    light_ratio = (LB / LA).value
+    J = mission.expected_brightness_ratio(T_eff_A, T_eff_B)
+
+    # Lookup the LD coeffs. Minimum supported T_eff for pow2 coeffs is 350 0K
+    LD_ALGO = "pow2"
+    ld_coeffs_A = limb_darkening.lookup_tess_pow2_ld_coeffs(loggA, max(T_eff_A, 3500 * u.K))
+    ld_coeffs_B = limb_darkening.lookup_tess_pow2_ld_coeffs(loggB, max(T_eff_B, 3500 * u.K))
+
+    if verbose:
+        print(f"""
+{label}:
+    Star A: M={MA:.3f}, R={RA:.3f}, T_eff={T_eff_A:.1f}, L={LA:.3f}, log(g)={loggA:.3f}, LD({LD_ALGO})={ld_coeffs_A}
+    Star B: M={MB:.3f}, R={RB:.3f}, T_eff={T_eff_B:.1f}, L={LB:.3f}, log(g)={loggB:.3f}, LD({LD_ALGO})={ld_coeffs_B}
+    system: age={age}, z={z}, y={y}, q={q:.3f}, k={k:.3f}, J={J:.3f}, LB/LA={light_ratio:.3f}""")
+
+    for (P, inc, ecc, omega, L3) in product(periods, incs, eccs, omegas, L3s):
+        # No point in varying omega when eccentricity is zero. This logic
+        # depends on there being e and omega values of zero in the pspace lists.
+        generated_counter += 1
+        if ecc != 0. or (ecc == 0. and omega == 0.):
+            a = orbital.semi_major_axis(MA, MB, P)
+            rA = (RA.to(u.solRad) / a.to(u.solRad)).value
+            rB = (RB.to(u.solRad) / a.to(u.solRad)).value
+            omega_rad = omega.to(u.rad).value
+            esinw = ecc * np.sin(omega_rad)
+            ecosw = ecc * np.cos(omega_rad)
+            imp_prm = orbital.impact_parameter(rA, inc, ecc, None, esinw, orbital.EclipseType.BOTH)
+
+            pset = {
+                "id":           f"{generated_counter:06d}",
+
+                # Basic system params for generating the model light-curve
+                # The keys (& those for LD below) are those expected by make_dateset
+                "rA_plus_rB":   rA + rB,
+                "k":            k,
+                "inc":          inc.to(u.deg).value, 
+                "qphot":        q,
+                "ecosw":        ecosw,
+                "esinw":        esinw,
+                "J":            J,
+                "L3":           L3,
+
+                **_def_limb_darkening_params,
+
+                # Further params for potential use as labels/features
+                "rA":           rA,
+                "rB":           rB,
+                "e":            ecc,
+                "omega":        omega.to(u.deg).value,
+                "bP":           imp_prm[0],
+                "bS":           imp_prm[1],
+
+                # For reference: physical params.
+                "LB_over_LA":   light_ratio,
+                "P":            P.to(u.d).value,                   
+                "a":            a.to(u.solRad).value,
+                "RA":           RA.to(u.solRad).value,
+                "RB":           RB.to(u.solRad).value,
+                "MA":           MA.to(u.solMass).value,
+                "MB":           MB.to(u.solMass).value
+            }
+
+            if _is_usable_system(pset):
+                yield pset
+                usable_counter += 1
+                if verbose and usable_counter % 100 == 0:
+                    print(f"{label}: Generated {usable_counter:,} usable",
+                        f"instances from {generated_counter:,} distinct configurations.")
 
 
 def _calculate_file_splits(instance_count: int, file_count: int) -> List[int]:
