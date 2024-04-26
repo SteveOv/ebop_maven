c758cbebccabbfe3fe7c3daf99c731c5b3b305f0
SteveOv <steveoverall@hotmail.co.uk>
2024-04-26 13:14:47 +0100

    added verbosity & pooling_layer() to modelling module

--- ebop_maven/modelling.py
+++ ebop_maven/modelling.py
@@ -3,6 +3,7 @@ from typing import Union, List, Dict, Tuple, Callable
 from pathlib import Path
 
 from keras import layers, models, KerasTensor
+from keras.src.layers.pooling.base_pooling import BasePooling
 
 from .libs import deb_example
 
@@ -54,7 +55,8 @@ def conv1d_layers(num_layers: int=1,
                   strides: Union[int, List[int]]=2,
                   padding: Union[str, List[str]]="same",
                   activation: Union[any, List[any]]="relu",
-                  name_prefix: str="CNN-") -> KerasTensor:
+                  name_prefix: str="Conv-",
+                  verbose: bool=True) -> KerasTensor:
     """
     Builds the requested set of Conv1D layers, returning the output tensor of the last layer built.
     The filters, kernel_size, strides, padding and activation arguments can be a List of values,
@@ -67,6 +69,7 @@ def conv1d_layers(num_layers: int=1,
     :padding: the padding value of each layer
     :activation: the activation value of each layer
     :name_prefix: the text to prefix the indexed layer name
+    :verbose: print out info of what's happening
     :returns: the output tensor of the last new layer
     """
     # pylint: disable=too-many-arguments
@@ -84,14 +87,46 @@ def conv1d_layers(num_layers: int=1,
     def layers_func(input_tensor: KerasTensor) -> KerasTensor:
         # Expected failure if any list isn't num_layers long
         for ix in range(num_layers):
-            input_tensor = layers.Conv1D(filters=filters[ix],
+            name = f"{name_prefix}{ix+1}"
+            output_tensor = layers.Conv1D(filters=filters[ix],
                                          kernel_size=kernel_size[ix],
                                          strides=strides[ix],
                                          padding=padding[ix],
                                          activation=activation[ix],
-                                         name=f"{name_prefix}{ix+1}")(input_tensor)
-        return input_tensor
+                                         name=name)(input_tensor)
+            if verbose:
+                print(f"Creating Conv1D('{name}', filters={filters[ix]},",
+                      f"kernel_size={kernel_size[ix]}, strides={strides[ix]},",
+                      f"padding={padding[ix]}, activation={activation[ix]})",
+                      f"({input_tensor.shape}) -> {output_tensor.shape}")
+            input_tensor = output_tensor
+        return output_tensor
     return layers_func
+
+
+def pooling_layer(pool_type: BasePooling,
+                  pool_size: int=2,
+                  strides: int=2,
+                  name: str=None,
+                  verbose: bool=True) -> KerasTensor:
+    """
+    Builds the requested Pooling layer, returning its output tensor.
+
+    :pool_type: the type of layer
+    :pool_size: the pool_size of the layer
+    :strides: the strides value of the layer
+    :name: the name of the layer
+    :verbose: print out info of what's happening
+    :returns: the output tensor of the new layer
+    """
+    def layer_func(input_tensor: KerasTensor) -> KerasTensor:
+        output_tensor = pool_type(pool_size=pool_size, strides=strides, name=name)(input_tensor)
+        if verbose:
+            print(f"Creating {pool_type.__name__}('{name}',"
+                  f"pool_size={pool_size}, strides={strides})",
+                  f"({input_tensor.shape}) -> {output_tensor.shape}")
+        return output_tensor
+    return layer_func
 
 
 def hidden_layers(num_layers: int=1,
@@ -99,7 +134,8 @@ def hidden_layers(num_layers: int=1,
                   kernel_initializer: Union[str, List[str]]="glorot_uniform",
                   activation: Union[any, List[any]]="relu",
                   dropout_rate: Union[float, List[float]]=0,
-                  name_prefix: Tuple[str, str]=("Hidden-", "Dropout-")) -> KerasTensor:
+                  name_prefix: Tuple[str, str]=("Hidden-", "Dropout-"),
+                  verbose: bool=True) -> KerasTensor:
     """
     Builds the requested set of hidden Dense layers with optional accompanying Dropout layers,
     returning the output tensor of the last layer built. The units, activation, kernel_initializer
@@ -112,6 +148,7 @@ def hidden_layers(num_layers: int=1,
     :activation: the activation function for each new layer
     :dropout_rate: the fraction of the units to drop for each layer. No dropout layer when set to 0
     :name_prefix: the text to prefix the indexed layer name
+    :verbose: print out info of what's happening
     :returns: the output tensor of the last new layer
     """
     # pylint: disable=too-many-arguments
@@ -127,45 +164,66 @@ def hidden_layers(num_layers: int=1,
     def layers_func(input_tensor: KerasTensor) -> KerasTensor:
         # Expected failure if any list isn't num_layers long
         for ix in range(num_layers):
-            input_tensor = layers.Dense(units[ix],
-                                        kernel_initializer=kernel_initializer[ix],
-                                        activation=activation[ix],
-                                        name=f"{name_prefix[0]}{ix+1}")(input_tensor)
+            name = f"{name_prefix[0]}{ix+1}"
+            output_tensor = layers.Dense(units[ix],
+                                         kernel_initializer=kernel_initializer[ix],
+                                         activation=activation[ix],
+                                         name=name)(input_tensor)
+            if verbose:
+                print(f"Creating Dense('{name}', units={units[ix]},",
+                      f"kernel_initializer={kernel_initializer[ix]},",
+                      f"activation={activation[ix]})",
+                      f"({input_tensor.shape}) -> {output_tensor.shape}")
+
+            input_tensor = output_tensor
             if dropout_rate[ix]:
-                input_tensor = layers.Dropout(dropout_rate[ix],
-                                              name=f"{name_prefix[1]}{ix+1}")(input_tensor)
-        return input_tensor
+                name = f"{name_prefix[1]}{ix+1}"
+                output_tensor = layers.Dropout(dropout_rate[ix], name=name)(input_tensor)
+                if verbose:
+                    print(f"Creating Dropout('{name}', rate={dropout_rate[ix]})",
+                          f"({input_tensor.shape}) -> {output_tensor.shape}")
+                input_tensor = output_tensor
+        return output_tensor
     return layers_func
 
 
 def mags_input_layer(shape: Tuple[int, int]=(deb_example.mags_bins, 1),
-                     name: str="Mags-Input") -> KerasTensor:
+                     name: str="Mags-Input",
+                     verbose: bool=True) -> KerasTensor:
     """
     Builds a standard mags-feature input layer.
 
     :shape: the shape of the input tensors
     :name: the name of the layer
+    :verbose: print out info of what's happening
     :returns: the output tensor of the input layer
     """
+    if verbose:
+        print(f"Creating Input('{name}', shape={shape})")
     return layers.Input(shape=shape, name=name)
 
 
 def ext_input_layer(shape: Tuple[int, int]=(len(deb_example.extra_features_and_defaults), 1),
-                    name: str="Ext-Input") -> KerasTensor:
+                    name: str="Ext-Input",
+                    verbose: bool=True) -> KerasTensor:
     """
     Builds a standard ext-features input layer.
 
     :shape: the shape of the input tensors
     :name: the name of the layer
+    :verbose: print out info of what's happening
     :returns: the output tensor of the input layer
     """
+    if verbose:
+        print(f"Creating Input('{name}', shape={shape})")
     return layers.Input(shape=shape, name=name)
 
 
 def output_layer(label_names_and_scales: Dict[str, any]=None,
                  kernel_initializer: str="glorot_uniform",
                  activation: str="linear",
-                 name: str="Output") -> KerasTensor:
+                 name: str="Output",
+                 verbose: bool=True) -> KerasTensor:
     """
     Builds the requested output layer, returning its output tensor.
 
@@ -173,29 +231,37 @@ def output_layer(label_names_and_scales: Dict[str, any]=None,
     :kernel_initializer: the initializer
     :activation: the activation function
     :name: the name of the layer
+    :verbose: print out info of what's happening
     :returns: the output tensor of the new layer
     """
     if not label_names_and_scales:
         label_names_and_scales = deb_example.labels_and_scales
     def layer_func(input_tensor: KerasTensor) -> KerasTensor:
-        return OutputLayer(units=len(label_names_and_scales),
-                           kernel_initializer=kernel_initializer,
-                           activation=activation,
-                           label_names_and_scales=label_names_and_scales,
-                           name=name)(input_tensor)
+        units = len(label_names_and_scales)
+        output_tensor = OutputLayer(units=units,
+                                    kernel_initializer=kernel_initializer,
+                                    activation=activation,
+                                    label_names_and_scales=label_names_and_scales,
+                                    name=name)(input_tensor)
+        if verbose:
+            print(f"Creating OutputLayer('{name}', units={units},",
+                  f"kernel_initializer={kernel_initializer}, activation={activation},",
+                  f"label_names_and_scales={label_names_and_scales})",
+                  f"({input_tensor.shape}) -> {output_tensor.shape}")
+        return output_tensor
     return layer_func
-
 
 
 def build_mags_ext_model(
         name: str="Mags-Ext-Model",
-        mags_input: KerasTensor=mags_input_layer(),
-        ext_input: KerasTensor=ext_input_layer(),
+        mags_input: KerasTensor=mags_input_layer(verbose=False),
+        ext_input: KerasTensor=ext_input_layer(verbose=False),
         mags_layers: List[Callable[[KerasTensor], KerasTensor]]=None,
         ext_layers: List[Callable[[KerasTensor], KerasTensor]]=None,
         dnn_layers: List[Callable[[KerasTensor], KerasTensor]]=None,
-        output: Callable[[KerasTensor], KerasTensor]=output_layer(),
-        post_build_step: Callable[[models.Model], None]=None
+        output: Callable[[KerasTensor], KerasTensor]=output_layer(verbose=False),
+        post_build_step: Callable[[models.Model], None]=None,
+        verbose: bool=True
     ) -> models.Model:
     """
     Builds a multiple input model with separate Mags-Feature and Extra-Features inputs.
@@ -211,6 +277,7 @@ def build_mags_ext_model(
     outputs of the mags and ext layers
     :output: the output layer for publishing the output of the dnn_layers
     :post_build_step: optional hook to interact with the newly built model
+    :verbose: print out info of what's happening
     :returns: the new model
     """
     # pylint: disable=too-many-arguments
@@ -235,6 +302,10 @@ def build_mags_ext_model(
                     layers.Flatten(name="Mags-Reshape")(mags_tensor),
                     layers.Flatten(name="Ext-Reshape")(ext_tensor)
                 ])
+    if verbose:
+        print( "Creating Concatenate('DNN-Input', axis=1)",
+              f"([Flatten('Mags-Reshape')({mags_tensor.shape}),"
+              f" Flatten('Ext-Reshape')({ext_tensor.shape})]) -> {output_tensor.shape}")
 
     if dnn_layers:
         if not isinstance(dnn_layers, List):
@@ -246,6 +317,8 @@ def build_mags_ext_model(
     model = models.Model(inputs=[mags_input, ext_input], outputs=output_tensor, name=name)
 
     if post_build_step:
+        if verbose:
+            print(f"Calling out to the post_build_step: {post_build_step}")
         post_build_step(model)
 
     return model
--- make_trained_cnn_model.py
+++ make_trained_cnn_model.py
@@ -66,16 +66,18 @@ def make_best_model():
     Helper function for building the current best performing model. 
     Publish model from a function, rather than inline, so it can be shared with model_search.
     """
-    print("\nBuilding the best known CNN model for predicting:", ", ".join(CHOSEN_LABELS), "\n")
-    return modelling.build_mags_ext_model(
+    print("\nBuilding the best known CNN model for predicting:", ", ".join(CHOSEN_LABELS))
+    best_model = modelling.build_mags_ext_model(
         name=MODEL_NAME,
+        mags_input=modelling.mags_input_layer(shape=(MAGS_BINS, 1)),
+        ext_input=modelling.ext_input_layer(shape=(NUM_EXT_INPUTS, 1)),
         mags_layers=[
             modelling.conv1d_layers(2, 64, 4, 2, CNN_PADDING, CNN_ACTIVATE, "Conv-1-"),
-            layers.MaxPool1D(pool_size=2, strides=2, name="Pool-1"),
+            modelling.pooling_layer(layers.MaxPool1D, pool_size=2, strides=2, name="Pool-1"),
             modelling.conv1d_layers(2, 64, 4, 2, CNN_PADDING, CNN_ACTIVATE, "Conv-2-"),
-            layers.MaxPool1D(pool_size=2, strides=2, name="Pool-2"),
+            modelling.pooling_layer(layers.MaxPool1D, pool_size=2, strides=2, name="Pool-2"),
             modelling.conv1d_layers(2, 64, 4, 2, CNN_PADDING, CNN_ACTIVATE, "Conv-3-"),
-            layers.MaxPool1D(pool_size=2, strides=2, name="Pool-3"),
+            modelling.pooling_layer(layers.MaxPool1D, pool_size=2, strides=2, name="Pool-3"),
             modelling.conv1d_layers(2, 64, 4, 2, CNN_PADDING, CNN_ACTIVATE, "Conv-4-")
         ],
         dnn_layers=[
@@ -86,7 +88,9 @@ def make_best_model():
             modelling.hidden_layers(1, 64, DNN_INITIALIZER, DNN_ACTIVATE, 0, ("Taper-",))
         ],
         output=modelling.output_layer({l: deb_example.labels_and_scales[l] for l in CHOSEN_LABELS},
-                                    DNN_INITIALIZER, "linear", "Output"))
+                                      DNN_INITIALIZER, "linear", "Output"))
+    print(f"Have built the model {best_model.name}\n")
+    return best_model
 
 
 if __name__ == "__main__":
