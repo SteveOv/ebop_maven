6af4c156baef21accdb898b65ab71c0035ab8f30
SteveOv <steveoverall@hotmail.co.uk>
2025-08-28 22:39:24 +0100

    better error label still!

--- model_testing.py
+++ model_testing.py
@@ -897,10 +897,10 @@ def predictions_vs_labels_to_table(predictions: np.ndarray[UFloat],
     if print_it:
         to = StringIO()
 
-    # The relative error type will be equivalent to |(lbl-pred)/lbl|. The dont_div params are those
-    # we don't divide by lbl in this calc, as they're centred on zero & range from 0 to +/-1.
+    # The final, relative error type is equivalent to |(lbl-pred)/lbl|. The dont_div params are
+    # those we don't divide by lbl in this calc, as they're centred on zero & range from 0 to +/-1.
     dont_div = ["ecosw", "esinw"]
-    err_labels = ["MAE", "MSE", "R'tive Err"]
+    err_labels = ["MAE", "MSE", "Error"]
     line_length = 12 + (11 * len(selected_param_names)) + (11 * len(err_labels))
     def horizontal_line(char):
         to.write(char*line_length + "\n")
